70250682d1d443e1e52ce22ebb473a3bd0bfa262
Lucas Santos <leos.developer@gmail.com>
2022-12-04 02:29:32 -0300

    :pushpin: finished config routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,16 @@
-import React from "react"
+import { FC } from "react"
 import { Route, Routes } from "react-router-dom"
 import { Layout } from "../layouts"
 import { routes } from "./paths"
 
-const PathRoutes: React.FC = () => {
-    return (
-        <Routes>
-            {/* <Route path="*" element={<NotFound />} /> */}
-            <Route element={<Layout />}>
-                { routes.map(({ id, path, element }) => (
-                    <Route key={ id } element={ element } path={ path } />
-                )) }
-            </Route>
-        </Routes>
-    )
+export const PathRoutes: FC = () => {
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        { routes.map(({ id, path, element }) => (
+          <Route key={ id } element={ element } path={ path } />
+        )) }
+      </Route>
+    </Routes>
+  )
 }
-
-export default PathRoutes
