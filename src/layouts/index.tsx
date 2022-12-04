7257eb9cf8088737d45baf3f090408ad3c1aff09
Lucas Santos <leos.developer@gmail.com>
2022-12-04 02:32:43 -0300

    :pushpin: up updating codes development

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,11 @@
 import { FC } from "react"
+import { Outlet } from "react-router-dom"
 
 export const Layout: FC = () => {
-    return <></>
+    return (
+        <div className="container mx-auto px-8">
+
+            <Outlet />
+        </div>
+    )
 }
