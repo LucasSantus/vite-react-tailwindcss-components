69ddfd31fc71b1806999cadf03a5f46cf34963bf
Lucas Santos <leos.developer@gmail.com>
2022-12-28 10:02:27 -0300

    :pushpin: create a component dropdown [ src/pages/Dashboard/index.tsx ]

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,17 +1,29 @@
-import {
-  Article,
-  CheckCircle,
-  CodeSimple,
-  Info,
-  Trash,
-  WarningOctagon,
-} from "phosphor-react";
 import { FC, useState } from "react";
-import { ModalContent } from "../../components/modal/ModalContent";
-import { ModalInformation } from "../../components/modal/ModalInformation";
-import { Table } from "../../components/Table";
-import { datas } from "../../mocks/datas";
+import { Dropdown } from "../../components/Dropdown";
 
 export const Dashboard: FC = () => {
-  return <div></div>;
+  const [isActive, setIsActive] = useState(false);
+  return (
+    <div>
+      <Dropdown
+        isActive={isActive}
+        items={[
+          {
+            title: "Teste",
+            onClick: () => {
+              console.log("teste");
+            },
+          },
+        ]}
+      >
+        <button
+          className="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700"
+          type="button"
+          onClick={() => setIsActive((current) => !current)}
+        >
+          Dropdown
+        </button>
+      </Dropdown>
+    </div>
+  );
 };
