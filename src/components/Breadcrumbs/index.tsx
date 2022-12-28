2f8285f4ef488b96c1b8bd5efe95ae93bbdaa1b9
Lucas Santos <leos.developer@gmail.com>
2022-12-28 10:07:36 -0300

    :pushpin: implementing simple component breadcrumb [ src/components/Breadcrumbs/index.tsx ]

--- src/components/Breadcrumbs/index.tsx
+++ src/components/Breadcrumbs/index.tsx
@@ -16,7 +16,9 @@ export const Breadcrumbs: FC<IBreadcrumbProps> = ({ breads, simbol = "/" }) => {
 
   return (
     <div className="flex gap-2 text-slate-600 bg-slate-200 rounded-md px-3 py-2">
-      {breads.map(({ title, path }) => {
+      {breads.map(({ title, path }, index) => {
+        simbol = index != breads.length - 1 ? simbol : "";
+
         return (
           <a key={title} onClick={() => navigate(path)}>
             {title} {simbol}
